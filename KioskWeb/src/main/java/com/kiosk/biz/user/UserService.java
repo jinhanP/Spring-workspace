c89febdf3e81baa00ccbfb2051a29eaba9535652
ParkJinHan <145731370+jinhanP@users.noreply.github.com>
2024-02-01 23:10:57 +0900

    Delete KioskWeb directory

--- KioskWeb/src/main/java/com/kiosk/biz/user/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.kiosk.biz.user;
-
-public interface UserService {
-	
-	UserVO getUser(UserVO vo);
-
-}
--- KioskWeb/src/main/java/com/kiosk/view/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.kiosk.view.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-// 컨트롤은 이미 만들어져 있어서 지워도 되고 안지워도된다.
-public interface Controller {
-	String handleRequest(HttpServletRequest request , HttpServletResponse response);
-}
